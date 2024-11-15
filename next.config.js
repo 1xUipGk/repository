0d6a9a999dbddcd2edb996457a0888404dfb24ca
Ali Ismaeel <ali.ismaeel@example.com>
2024-11-15 14:19:13 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -21,31 +21,6 @@ const nextConfig = {
       fs: false,
     };
     return config;
-  },
-  experimental: {
-    esmExternals: 'loose'
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "style-src 'self' 'unsafe-inline'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:",
-              "connect-src 'self' http://localhost:1337 https://*.googleapis.com https://identitytoolkit.googleapis.com https://*.firebaseio.com https://securetoken.googleapis.com",
-              "img-src 'self' http://localhost:1337 data: blob:",
-              "media-src 'self' http://localhost:1337",
-              "font-src 'self' data:",
-              "frame-src 'self' http://localhost:1337"
-            ].join('; ')
-          }
-        ]
-      }
-    ];
   }
 };
 
