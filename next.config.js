e9766171cf2175307c2913c265d0182d75703191
Ali Ismaeel <ali.ismaeel@example.com>
2024-11-18 18:11:12 +0300

    Initial deployment

--- next.config.js
+++ next.config.js
@@ -7,6 +7,20 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/repository' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/repository/' : '',
+  swcMinify: false,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+  distDir: process.env.NODE_ENV === 'development' ? '.next' : 'out',
+  generateBuildId: async () => {
+    return 'build-' + Date.now();
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  }
 }
 
 module.exports = nextConfig
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,24 +1,19 @@
-import { initializeApp } from '@firebase/app';
+import { getApps, initializeApp } from '@firebase/app';
 import { Database, getDatabase, ref as dbRef, get as dbGet } from '@firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6tkZjsM5I4OkG8SvCk6v5pyeM6nTzGT4",
-  authDomain: "task-manager-87577.firebaseapp.com",
-  databaseURL: "https://task-manager-87577-default-rtdb.firebaseio.com",
-  projectId: "task-manager-87577",
-  storageBucket: "task-manager-87577.firebasestorage.app",
-  messagingSenderId: "949071438979",
-  appId: "1:949071438979:web:d2a0056c95b9e02e90e7b6",
-  measurementId: "G-HMZ54Z29EZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-let database: Database | null = null;
-
-try {
-  const app = initializeApp(firebaseConfig);
-  database = getDatabase(app);
-} catch (error) {
-  console.error('Error initializing Firebase:', error);
-}
+// تجنب تهيئة التطبيق أكثر من مرة
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+const database = getDatabase(app);
 
 export { database, dbRef as ref, dbGet as get }; 
