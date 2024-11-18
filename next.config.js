f7d2a0eb4f1b7daa6dec6fca0788d27f6ac3955b
Ali Ismaeel <ali.ismaeel@example.com>
2024-11-18 18:35:54 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -3,24 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    domains: ['blogger.googleusercontent.com', 'www.blogger.com']
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/repository' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/repository/' : '',
-  swcMinify: false,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.minimize = false;
-    }
-    return config;
-  },
-  distDir: process.env.NODE_ENV === 'development' ? '.next' : 'out',
-  generateBuildId: async () => {
-    return 'build-' + Date.now();
-  },
-  experimental: {
-    forceSwcTransforms: true,
-  }
+  basePath: '/repository',
+  assetPrefix: '/repository/',
 }
 
 module.exports = nextConfig
