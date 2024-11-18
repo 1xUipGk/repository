674387a1994bfb462359b526f9d85a38a94318f8
Ali Ismaeel <ali.ismaeel@example.com>
2024-11-18 17:49:30 +0300

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,10 +5,8 @@ const nextConfig = {
     unoptimized: true,
     domains: ['blogger.googleusercontent.com', 'www.blogger.com']
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/repository-main' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/repository-main/' : '',
-  reactStrictMode: true,
-  swcMinify: false
+  basePath: process.env.NODE_ENV === 'production' ? '/repository' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/repository/' : '',
 }
 
 module.exports = nextConfig
